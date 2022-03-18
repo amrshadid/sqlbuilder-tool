26afe48c55c21b85f326e28890e48bac049c4f9e
Amr Shadid <amr.shadid.2016@gmail.com>
2022-03-18 18:30:20 +0200

    change design structure on App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,20 @@
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
 import './App.css';
 import {AppProvider} from '../context'
 import Main from './main'
 import Sidebar from './navbar'
 
-
 export default function App(props: IAppProps) {
+
   return (
     <AppProvider>
-      <Container fluid>
+      <>
         <Sidebar/>
-
-        <Container>
-        <Main />
-        </Container>
-
-      </Container>
+        <>
+          <Main />
+        </>
+      </>
     </AppProvider>
 
   );
